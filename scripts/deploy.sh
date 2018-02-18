f33b1d94a75e99445ab840a588e5ce4dc587d35e
Jonathan Harrison <jonjam1990@googlemail.com>
2018-02-18 00:35:29 +0000

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,4 +11,6 @@ IFS=$'\n\t'
 
 aksName=$(./scripts/deploy-rg.sh -u $servicePrincipalId -s $servicePrincipalSecret -t $tenant -g $resourceGroupName -l $resourceGroupLocation -d $templateFilePath -p $parametersFilePath -q properties.outputs.aksName.value aksAgentAdminUsername=$aksAgentAdminUsername aksAgentSshRSAPublicKey="$aksAgentSshRSAPublicKey" aksServicePrincipalClientId=$aksServicePrincipalClientId aksServicePrincipalClientSecret=$aksServicePrincipalClientSecret)
 
-./scripts/install-helm.sh -u $servicePrincipalId -s $servicePrincipalSecret -t $tenant -g $resourceGroupName -l $resourceGroupLocation -c $aksName
+echo $aksName
+
+./scripts/install-helm.sh -u $servicePrincipalId -s $servicePrincipalSecret -t $tenant -g $resourceGroupName -c $aksName
